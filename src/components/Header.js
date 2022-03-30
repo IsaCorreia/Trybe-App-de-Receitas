9c3843a4f1391df883b250e5a8af0e368f9b1a12
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-03-30 18:59:42 -0300

    feat: add buttons, icons and respective data-testids to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 const Header = () => (
   <>
     <h1>Header!</h1>
-    <span>Search</span>
-    <span>Title</span>
-    <Link to="/profile">Profile</Link>
-    <span>Filters</span>
+    <button type="button">
+      <img data-testid="search-top-btn" src={ searchIcon } alt="seach" />
+    </button>
+    <span data-testid="page-title">Title(dinâmico de acordo com a página)</span>
+    <Link to="/profile">
+      <button type="button">
+        <img data-testid="profile-top-btn" src={ profileIcon } alt="profile" />
+      </button>
+    </Link>
+    <button type="button">Filters</button>
+    <hr />
   </>
 );
 
