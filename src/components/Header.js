ad09443cf965846a03be6d96697b105468475eb7
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-03-31 16:34:53 -0300

    feat: add conditional rendering to search input at the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,53 @@
 import propTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-const Header = ({ currentPage = 'Title', disableSearch = false }) => (
-  <div
-    style={ {
-      textAlign: 'center',
-      flexDirection: 'column',
-    } }
-  >
+const Header = ({ currentPage, disableSearch }) => {
+  const [disableSearchInput, setDisableSearchInput] = useState(true);
+
+  return (
     <div
       style={ {
-        display: 'flex',
-        justifyContent: 'space-between',
-        backgroundColor: '#EA1D2C ',
+        textAlign: 'center',
+        flexDirection: 'column',
       } }
     >
-      {!disableSearch && (
-        <button type="button">
-          <img data-testid="search-top-btn" src={ searchIcon } alt="serach" />
-        </button>
-      )}
-      <span data-testid="page-title">{currentPage}</span>
-      <Link to="/profile">
-        <button type="button">
-          <img data-testid="profile-top-btn" src={ profileIcon } alt="profile" />
-        </button>
-      </Link>
+      <div
+        style={ {
+          display: 'flex',
+          justifyContent: 'space-between',
+          backgroundColor: '#EA1D2C ',
+        } }
+      >
+        {!disableSearch && (
+          <button
+            type="button"
+            onClick={ () => setDisableSearchInput(!disableSearchInput) }
+          >
+            <img data-testid="search-top-btn" src={ searchIcon } alt="search" />
+          </button>
+        )}
+        <span data-testid="page-title">{currentPage}</span>
+        <Link to="/profile">
+          <button type="button">
+            <img
+              data-testid="profile-top-btn"
+              src={ profileIcon }
+              alt="profile"
+            />
+          </button>
+        </Link>
+      </div>
+
+      { !disableSearchInput && <input type="text" data-testid="search-input" /> }
+
+      <button type="button">Filters</button>
+      <hr />
     </div>
-    <button type="button">Filters</button>
-    <hr />
-  </div>
-);
+  );
+};
 
 Header.propTypes = {
   currentPage: propTypes.string.isRequired,
