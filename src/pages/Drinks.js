1a10e64265ffd2a50bbb689eb8c16827d3bddf05
felipe5215 <felipe.eleotero@gmail.com>
2022-03-31 18:13:03 -0300

    feat: implementing initial request for drinks and foods

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
+import { DRINKS_ENDPOINT } from '../helpers/enpoints';
+import useFoodInitialRequest from '../hooks/useFoodInitialRequest';
 
-const Drinks = () => (
-  <>
-    <Header currentPage="Drinks" />
-    <h1>Drinks!</h1>
-    <Footer />
-  </>
-);
+const Drinks = () => {
+  const { setDrinksInitialRequest } = useContext(RecipesContext);
+  useFoodInitialRequest(DRINKS_ENDPOINT, setDrinksInitialRequest, 'drinks');
+  return (
+    <>
+      <Header currentPage="Drinks" />
+      <h1>Drinks!</h1>
+      <Footer />
+    </>
+  );
+};
 
 export default Drinks;
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
+import { MEALS_ENDPOINT } from '../helpers/enpoints';
+import useFoodInitialRequest from '../hooks/useFoodInitialRequest';
 
-const Foods = () => (
-  <>
-    <Header currentPage="Foods" />
-    <h1>Foods!</h1>
-    <Footer />
-  </>
-);
+const Foods = () => {
+  const { setFoodInitialRequest } = useContext(RecipesContext);
+  useFoodInitialRequest(MEALS_ENDPOINT, setFoodInitialRequest, 'foods');
+  return (
+    <>
+      <Header currentPage="Foods" />
+      <h1>Foods!</h1>
+      <Footer />
+    </>
+  );
+};
 
 export default Foods;
