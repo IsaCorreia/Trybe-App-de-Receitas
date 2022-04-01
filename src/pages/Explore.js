d35b2c9d0f928dcaf073f4ea23d64d0c618d2d73
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-04-01 17:58:36 -0300

    feat: add explore buttons to explore page

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +7,16 @@ const Explore = () => (
   <>
     <Header currentPage="Explore" disableSearch />
     <h1>Explore!</h1>
+    <Link to="/explore/foods">
+      <button type="button" data-testid="explore-foods">
+        Explore Foods
+      </button>
+    </Link>
+    <Link to="/explore/drinks">
+      <button type="button" data-testid="explore-drinks">
+        Explore Drinks
+      </button>
+    </Link>
     <Footer />
   </>
 );
