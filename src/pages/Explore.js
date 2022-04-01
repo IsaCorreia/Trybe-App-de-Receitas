0d2275ef8e0dba9b648986880ca758bd40a63835
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-04-01 18:09:18 -0300

    style: add positioning to explore buttons

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -6,17 +6,27 @@ import Header from '../components/Header';
 const Explore = () => (
   <>
     <Header currentPage="Explore" disableSearch />
-    <h1>Explore!</h1>
-    <Link to="/explore/foods">
-      <button type="button" data-testid="explore-foods">
-        Explore Foods
-      </button>
-    </Link>
-    <Link to="/explore/drinks">
-      <button type="button" data-testid="explore-drinks">
-        Explore Drinks
-      </button>
-    </Link>
+
+    <div
+      style={ {
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'center',
+      } }
+    >
+      <h1>Explore!</h1>
+      <Link to="/explore/foods">
+        <button type="button" data-testid="explore-foods">
+          Explore Foods
+        </button>
+      </Link>
+      <Link to="/explore/drinks">
+        <button type="button" data-testid="explore-drinks">
+          Explore Drinks
+        </button>
+      </Link>
+    </div>
+
     <Footer />
   </>
 );
