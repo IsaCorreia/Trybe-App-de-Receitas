3e06734eecb908bee147f64251f45a73d13c0b6f
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-04-01 18:56:14 -0300

    feat: add buttons to explore drink/meals page

--- src/pages/DrinksExplore.js
+++ src/pages/DrinksExplore.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const DrinksExplore = () => (
-  <>
-    <Header currentPage="Explore Drinks" disableSearch />
-    <h1>DrinksExplore!</h1>
-    <Footer />
-  </>
-);
+const DrinksExplore = () => {
+  // Fazer requisição de cocktail aleatória
+  // Endpoint: https://www.thecocktaildb.com/api/json/v1/1/random.php
+  const randomRecipeId = '1';
+  return (
+    <>
+      <Header currentPage="Explore Drinks" disableSearch />
+      <div
+        style={ {
+          display: 'flex',
+          flexDirection: 'column',
+          textAlign: 'center',
+        } }
+      >
+        <h1>DrinksExplore!</h1>
+        <Link to="/explore/drinks/ingredients">
+          <button type="button" data-testid="explore-by-ingredient">
+            By Ingredient
+          </button>
+        </Link>
+        <Link to={ `/drinks/${randomRecipeId}` }>
+          <button type="button" data-testid="explore-surprise">
+            Surprise me!
+          </button>
+        </Link>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default DrinksExplore;
