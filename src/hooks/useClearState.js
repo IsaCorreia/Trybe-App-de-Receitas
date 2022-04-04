2b68ac7b696817c07052f7d83b783bcc1f757492
felipe5215 <felipe.eleotero@gmail.com>
2022-04-04 15:36:52 -0300

    fix: lint syles

--- src/hooks/useClearState.js
+++ src/hooks/useClearState.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from 'react';
 import RecipesContext from '../context/RecipesContext';
 
+// limpa estado quando desmonta componente
 const useClearState = () => {
   const { setCurrentFilter } = useContext(RecipesContext);
   useEffect(() => {
