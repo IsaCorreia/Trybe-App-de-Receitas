f1f5b28301864d05ee5245b228485b57ea5c031a
eduardojigub <eduardobrunoit@gmail.com>
2022-04-03 22:03:18 -0300

    feat: Profile screen

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,30 +1,67 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const Profile = () => (
-  <>
-    <Header currentPage="Profile" />
+const Profile = ({ history }) => {
+  const getLocalStorageEmail = () => {
+    const getUser = JSON.parse(localStorage.getItem('user'));
+    return getUser.email;
+  };
 
-    <h1>Profile!</h1>
-    <div style={ { display: 'flex', flexDirection: 'column', textAlign: 'center' } }>
-      <span>Email</span>
-      <Link to="/favorite-recipes">
-        <button type="button">FavoriteRecipes</button>
-      </Link>
-      <Link to="/done-recipes">
-        <button type="button">DoneRecipes</button>
-      </Link>
-      <Link to="/">
-        <button type="button" onClick={ () => global.alert('Saiu') }>
+  const redirectLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  return (
+    <>
+      <Header currentPage="Profile" />
+      <h1>Profile!</h1>
+      <div
+        style={ { display: 'flex',
+          flexDirection: 'column',
+          textAlign: 'center',
+          alignItems: 'center' } }
+      >
+        <span
+          data-testid="profile-email"
+        >
+          { getLocalStorageEmail() }
+
+        </span>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ redirectLogout }
+        >
           Logout
         </button>
-      </Link>
-    </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-    <Footer />
-  </>
-);
+Profile.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Profile;
