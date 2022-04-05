62301a3abd7b3bce8e2ee902d8deb095952bc846
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-04-05 20:15:52 -0300

    feat: add more functionality to ingredient card component

--- src/components/IngredientCard.js
+++ src/components/IngredientCard.js
@@ -1,18 +1,47 @@
 import PropTypes from 'prop-types';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import AppContext from '../context/AppContext';
+import RecipesContext from '../context/RecipesContext';
+import { fetchFoodByIngredient } from '../services/fetchFoods';
 
-function IngredientCard({ index, ingredientName, ingredientImage }) {
+function IngredientCard({ index, ingredientName, ingredientImage, type }) {
+  const { setRecipesAPI } = useContext(AppContext);
+  const { setFoodRequest } = useContext(RecipesContext);
+  const history = useHistory();
+
+  const verifyIfRecipeFound = (recipes) => {
+    if (!recipes) {
+      global.alert('Sorry, we haven\'t found any recipes for these filters.');
+    }
+  };
+
+  const handleClick = async () => {
+    let recipes = [];
+    if (type === 'foods') {
+      recipes = await fetchFoodByIngredient(ingredientName);
+      verifyIfRecipeFound(recipes);
+      setRecipesAPI(recipes);
+      setFoodRequest(recipes);
+    }
+    history.push(`/${type}`, { from: `/explore/${type}/ingredients` });
+  };
   return (
-    <div data-testid={ `${index}-ingredient-card` } className="card-recipe">
-      <Link to="/">
+    <div
+      data-testid={ `${index}-ingredient-card` }
+      className="card-recipe"
+    >
+      <button
+        type="button"
+        onClick={ handleClick }
+      >
         <img
           src={ ingredientImage }
           alt={ ingredientName }
           data-testid={ `${index}-card-img` }
         />
         <p data-testid={ `${index}-card-name` }>{ingredientName}</p>
-      </Link>
+      </button>
     </div>
   );
 }
@@ -21,6 +50,7 @@ IngredientCard.propTypes = {
   index: PropTypes.number.isRequired,
   ingredientName: PropTypes.string.isRequired,
   ingredientImage: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
 
 export default IngredientCard;
