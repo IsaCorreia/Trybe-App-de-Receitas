c32d440b3d6f443e9caf27d70a8732ff4e065240
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-05 18:14:30 -0300

    feat: Implementation of the food details page. Data distribution.

--- src/pages/FoodDetail.js
+++ src/pages/FoodDetail.js
@@ -2,19 +2,18 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import useRecipeInitialRequest from '../hooks/useRecipeInitialRequest';
 import RecipeContext from '../context/RecipesContext';
+import reducerRecipeDetail from '../helpers/reduceRecipeDetails';
 
 const FoodDetail = ({
   match: {
     params: { id },
   },
 }) => {
-  const DETAILS_END_POINT = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  const FOOD_DETAILS_URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
 
   const { foodDetail, setFoodDetail } = useContext(RecipeContext);
 
-  useRecipeInitialRequest(DETAILS_END_POINT, setFoodDetail, 'meals');
-
-  console.log(foodDetail);
+  useRecipeInitialRequest(FOOD_DETAILS_URL, setFoodDetail, 'meals');
 
   if (!foodDetail) {
     return <p>Loading...</p>;
@@ -27,31 +26,33 @@ const FoodDetail = ({
     strInstructions,
     strYoutube,
   } = foodDetail[0];
-  // criar um array com os elementos da receita filatrando
-  // cada elemento por valor
 
   return (
     <main>
       <div>
-        <img src={ strMealThumb } alt={ strMeal } />
-        <h1>{ strMeal }</h1>
-        <h3>{ strCategory }</h3>
+        <img src={ strMealThumb } alt={ strMeal } data-testid="recipe-photo" />
+        <h1 data-testid="recipe-title">{ strMeal }</h1>
+        <h3 data-testid="recipe-category">{ strCategory }</h3>
       </div>
       <section>
         <h2>Ingtredients</h2>
-
         <ol>
-          <li>igrediente 1</li>
-          <li>igrediente 2</li>
-          <li>igrediente 3</li>
+          {reducerRecipeDetail(foodDetail[0]).map((value, index) => (
+            <li
+              key={ index }
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              { value }
+            </li>
+          ))}
         </ol>
       </section>
       <section>
-        <div>
-          { strInstructions }
+        <div data-testid="instructions">
+          {strInstructions}
         </div>
       </section>
-      <section>
+      <section data-testid="video">
         {strYoutube}
       </section>
     </main>
