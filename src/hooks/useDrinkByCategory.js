631546f2aa6b1d316fbc05ee8efdb3406dfe5e01
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-04-06 16:34:44 -0300

    feat: add new state to stop auto filtering upon entering respective page

--- src/hooks/useDrinkByCategory.js
+++ src/hooks/useDrinkByCategory.js
@@ -2,10 +2,10 @@ import { useContext, useEffect } from 'react';
 import RecipesContext from '../context/RecipesContext';
 
 const useDrinksByCategory = (endpoint, setRecipe, recipe) => {
-  const { currentFilter } = useContext(RecipesContext);
+  const { currentFilter, isFilterByCategoryOn } = useContext(RecipesContext);
 
   useEffect(() => {
-    if (currentFilter !== 'All') {
+    if (currentFilter !== 'All' && isFilterByCategoryOn) {
       const initialRequest = async () => {
         const category = await
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${currentFilter}`)
@@ -14,7 +14,7 @@ const useDrinksByCategory = (endpoint, setRecipe, recipe) => {
       };
       initialRequest();
     }
-    if (currentFilter === 'All') {
+    if (currentFilter === 'All' && isFilterByCategoryOn) {
       let selectedRecipe = recipe;
       const initialRequest = async () => {
         selectedRecipe = await fetch(endpoint).then(((response) => response.json()));
@@ -23,7 +23,7 @@ const useDrinksByCategory = (endpoint, setRecipe, recipe) => {
       };
       initialRequest();
     }
-  }, [currentFilter, endpoint, recipe, setRecipe]);
+  }, [currentFilter, endpoint, recipe, setRecipe, isFilterByCategoryOn]);
 };
 
 export default useDrinksByCategory;
