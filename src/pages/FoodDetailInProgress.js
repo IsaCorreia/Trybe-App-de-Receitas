2b5f0a04a4fec8fd83816731d9bc99da1f445f90
felipe5215 <felipe.eleotero@gmail.com>
2022-04-06 15:35:34 -0300

    nada

--- src/pages/FoodDetailInProgress.js
+++ src/pages/FoodDetailInProgress.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import RecipesContext from '../context/RecipesContext';
 import { RECIPE_DETAILS_ENDPOINT } from '../helpers/enpoints';
@@ -8,9 +8,46 @@ const FoodDetailInProgress = ({ location: { pathname } }) => {
   const SEVEN = 7;
   const TWELVE = 12;
   const ID = pathname.slice(SEVEN, TWELVE);
+
   const { recipeDetails, setRecipeDetails } = useContext(RecipesContext);
+  const [stateIngredient, setStateIngredient] = useState({
+    cocktails: {},
+    meals: {
+      [ID]: [] },
+  });
 
   useDetailsRequest(RECIPE_DETAILS_ENDPOINT(ID), setRecipeDetails, 'meals');
+
+  const handleIngredient = ({ target: { name } }) => {
+    if (stateIngredient.meals[ID] === undefined) {
+      setStateIngredient({
+        ...stateIngredient,
+        meals: {
+          ...stateIngredient.meals,
+          [ID]: [name],
+        },
+      });
+    } else {
+      setStateIngredient({
+        ...stateIngredient,
+        meals: {
+          ...stateIngredient.meals,
+          [ID]: [...stateIngredient.meals[ID], name],
+        },
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('inProgressRecipes')) {
+      const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'));
+      setStateIngredient(inProgressRecipes);
+    }
+  }, [ID]);
+
+  useEffect(() => {
+    localStorage.setItem('inProgressRecipes', JSON.stringify(stateIngredient));
+  }, [stateIngredient]);
 
   return (
     <div>
@@ -33,7 +70,6 @@ const FoodDetailInProgress = ({ location: { pathname } }) => {
             className="text-center text-muted text-lowercase"
           >
             {recipeDetails.strCategory}
-
           </p>
           <button
             data-testid="favorite-btn"
@@ -42,26 +78,27 @@ const FoodDetailInProgress = ({ location: { pathname } }) => {
             onClick={ () => console.log('compartilhou') }
           >
             Compartilhar
-
           </button>
           <hr />
-          {/* <div className="d-flex flex-column align-items-center mt-3">
-            {recipeDetails.ingredients.map((ingredient, index) => (
+          <div className="d-flex flex-column align-items-center mt-3">
+            {Object.values(recipeDetails.ingredients).map((ingredient, index) => (
               <label
                 className="text-muted text-lowercase"
                 key={ index }
-                htmlFor={ `ingredient-${index}` }
+                htmlFor={ `${index + 1}` }
               >
                 <input
                   data-testid={ `${index}-ingredient-step` }
                   className="ml-2 mr-2"
-                  name={ `ingredient-${index}` }
+                  name={ `${index + 1}` }
                   type="checkbox"
+                  checked={ stateIngredient.meals[ID].includes(`${index + 1}`) }
+                  onChange={ (e) => handleIngredient(e) }
                 />
-                { `${ingredient[0]}: ${ingredient[1]}` }
+                {`${Object.keys(ingredient)[0]} - ${Object.values(ingredient)[0]}`}
               </label>
             ))}
-          </div> */}
+          </div>
           <hr />
           <h3 className="text-center">Instructions</h3>
           <div className="d-flex flex-row justify-content-center">
