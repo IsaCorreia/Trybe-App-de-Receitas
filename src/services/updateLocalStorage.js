61b5cf572f3df8c957e208a1faa43a35a900c6b8
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-08 13:21:18 -0300

    feat: partial implementation of auxiliary data storage functions.

---
+++ src/services/updateLocalStorage.js
@@ -0,0 +1,20 @@
+const updateLocalStorage = (key, value) => {
+  const oldStorage = JSON.parse(localStorage.getItem(key));
+  if (oldStorage) {
+    const newStorage = [...oldStorage, value];
+    localStorage.setItem(key, JSON.stringify(newStorage));
+  } else {
+    localStorage.setItem(key, JSON.stringify([value]));
+  }
+};
+
+const newStorage = (key, value) => localStorage.setItem(key, JSON.stringify(value));
+const loadSotorage = (key) => JSON.parse(localStorage.getItem(key));
+
+const filterItemById = (key, id) => {
+  const oldList = JSON.parse(localStorage.getItem(key)) || [];
+  const newList = oldList.filter((recipe) => recipe.id !== id);
+  localStorage.setItem(key, JSON.stringify(newList));
+};
+
+export { updateLocalStorage, newStorage, filterItemById, loadSotorage };
